1bf3e9784701cff517e013185f11ff1efa4a6710
vukilis <vuklekic22@gmail.com>
2021-11-15 16:44:38 +0100

    zsh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#### Instalation ####
+# sudo apt install -y git zsh zsh-syntax-highlighting autojump zsh-autosuggestions
+# clone: git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+# echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+# chsh $USER
+#     /bin/zsh   # sudo -u $USER chsh -s /bin/zsh
+
+echo "--------------------------------------"
+echo "-----       ZSH INSTALATION      -----"
+echo "--------------------------------------"
+
+apt install -y git zsh zsh-syntax-highlighting autojump zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+chsh $USER -s /bin/zsh
+echo $SHELL
+echo "RESTART TERMINAL OR REBOOT PC"
+
+echo "--------------------------------------"
+echo "-----         ZSH SETUP          -----"
+echo "--------------------------------------"
